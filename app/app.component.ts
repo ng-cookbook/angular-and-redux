77ef4725a459f2b3492fdd488ff3002b3b4641e6
Dave Baskin <dfbaskin@gmail.com>
2016-11-29 21:57:13 -0500

    Adding four boxes.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,37 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello Angular</h1>`
+    template: `
+      <div>
+        <div>
+          <div>A</div>
+          <div>B</div>
+        </div>
+        <div>
+          <div>C</div>
+          <div>D</div>
+        </div>
+      </div>`,
+  styles: [
+`
+:host > div {
+  width: 75vw;
+  height: 75vh;
+  display: flex;
+  flex-direction: column;
+}
+:host > div > div {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: row;
+}
+:host > div > div > div {
+  border: 1px solid gray;
+  margin: 10px;
+  padding: 10px;
+  flex-grow: 1;
+}
+`
+  ]
 })
 export class AppComponent { }
